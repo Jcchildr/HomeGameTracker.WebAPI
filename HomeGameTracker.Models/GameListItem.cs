78c949d80b9f4a38cdca6d85cb7eedbda637d52a
Jcchildr <jcchildr@gmail.com>
2021-05-20 20:35:08 -0400

    updated data class

---
+++ HomeGameTracker.Models/GameListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeGameTracker.Models
+{
+    public class GameListItem
+    {
+        //returning information on the game when it's part of a list
+        public int GameId { get; set; }
+        [Display(Name="Title")]
+        public int GameName { get; set; }
+        [Display(Name = "Age Range")]
+        public string AgeRange { get; set; }
+        [Display(Name = "Number Of Players")]
+        [Range(1, 64, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public object NumberOfPlayers { get; set; }
+
+    }//end of GameListItem
+}
