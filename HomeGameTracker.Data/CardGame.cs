6dec5ce77c3254aaf1db8a1ad0afb955dadfb9c7
Soneil Pitamber <pitamber@att.net>
2021-05-25 21:03:08 -0400

    added cardGame class

--- HomeGameTracker.Data/CardGame.cs
+++ HomeGameTracker.Data/CardGame.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeGameTracker.Data
 {
-    class CardGame
+    public class CardGame : Game
     {
-
-    }
+        [Required]
+        public string NumberOfCards { get; set; }
+        [Required]
+        public string ExtraEquipmentUsed { get; set; }
+        [Required]
+        public bool IsGamblingGame { get; set; }
+        [Required]
+        public int AvgPlayTimeInMin { get; set; }
+    }//end of class CardGame
 }
