40f503c8ba4667524ccce0c8b31edbc6660e944d
Soneil Pitamber <pitamber@att.net>
2021-05-29 17:25:25 -0400

    not posting a cardGame yet

---
+++ HomeGameTracker.Models/CardGameCreate.cs
@@ -0,0 +1,22 @@
+﻿using HomeGameTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeGameTracker.Models
+{
+    public class CardGameCreate : Game
+    {
+        [Required]
+        public string NumberOfCards { get; set; }
+        [Required]
+        public string ExtraEquipmentUsed { get; set; }
+        [Required]
+        public bool IsGamblingGame { get; set; }
+        [Required]
+        public int AvgPlayTimeInMin { get; set; }
+    }
+}
---
+++ HomeGameTracker.Models/CardGameDetail.cs
@@ -0,0 +1,18 @@
+﻿using HomeGameTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeGameTracker.Models
+{
+    public class CardGameDetail : Game
+    {
+        public string NumberOfCards { get; set; }
+        public string ExtraEquipmentUsed { get; set; }
+        public bool IsGamblingGame { get; set; }
+        public int AvgPlayTimeInMin { get; set; }
+
+    }
+}
