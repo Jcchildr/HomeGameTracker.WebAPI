585215880e82cc37823a4f40fb2e0cc034386c48
Daniel Rogers <boldanrogers@gmail.com>
2021-06-03 17:37:07 -0400

    added data classes

---
+++ HomeGameTracker.Data/BoardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeGameTracker.Data
+{
+    public class BoardGame: Game
+    {
+        public int AveragePlayTimeMin { get; set; }
+        public String GameBoardType { get; set; }
+
+
+    }//end of class BoardGame
+}
---
+++ HomeGameTracker.Data/YardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeGameTracker.Data
+{
+    public class YardGame
+    {
+        public String SurfaceType { get; set; }
+        public Boolean BallGame { get; set; }
+        public int AreaOfPlayInFt { get; set; }
+
+    }//end of class YardGame
+}
