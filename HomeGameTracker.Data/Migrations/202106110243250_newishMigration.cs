5ee39f3ab91f9d148d561992d719c09f4d9c9e93
Daniel Rogers <boldanrogers@gmail.com>
2021-06-10 22:44:29 -0400

    added another newish migration to fix something or other

---
+++ HomeGameTracker.Data/Migrations/202106110243250_newishMigration.cs
@@ -0,0 +1,16 @@
+namespace HomeGameTracker.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newishMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
